07cff6b1c81452a239dfb3bd866478b78001183e
caesium72 <eka.caesium721@gmail.com>
2023-12-13 14:05:55 +0600

    code done in bubble binary sort

---
+++ Codes/binary.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // Check if the target is present at the middle
+        if (arr[mid] == target)
+            return mid;
+
+        // If the target is greater, ignore the left half
+        if (arr[mid] < target)
+            left = mid + 1;
+
+        // If the target is smaller, ignore the right half
+        else
+            right = mid - 1;
+    }
+
+    // If we reach here, then the element was not present
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 23;
+
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    if (result == -1)
+        printf("Element %d is not present in the array.\n", target);
+    else
+        printf("Element %d is present at index %d.\n", target, result);
+
+    return 0;
+}
